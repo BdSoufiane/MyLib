a07794ad46d6d2240bd9553847ddff838eb2b1aa
BdSoufiane <soufianbadda205@gmail.com>
2023-05-22 17:06:03 +0100

    Initial Author Routes

---
+++ models/author.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const authorShema=new mongoose.Schema({
+    name: {
+        type:String,
+        require:true
+    }
+})
+module.exports = mongoose.model('Author', authorShema)
--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app=express()
 const expressLayouts=require('express-ejs-layouts')
 
 const indexRouter=require('./routes/index')
-
+const authorsRouter=require('./routes/authors')
+const bodyParser=require('body-parser')
 
 
 app.set('view engine','ejs')
@@ -16,6 +17,7 @@ app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 // Mongoose
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
@@ -25,5 +27,6 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 
 app.use('/',indexRouter)
+app.use('/authors',authorsRouter)
 
 app.listen(process.env.PORT || 3000)
